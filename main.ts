0f4ccb14265cef6a1d0920ce7911b6ba92ac29d4
JaysonGonzales1272009 <125311522+JaysonGonzales1272009@users.noreply.github.com>
2023-02-16 08:59:43 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,91 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . . . #
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # # #
+        . . . # #
+        . . . . #
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . # # # #
+        . . # # #
+        . . . # .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . # . .
+        . # # # #
+        # # # # #
+        . # # # #
+        . . # . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . # . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . .
+        # # # # .
+        # # # # .
+        # # # # .
+        # . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        # # # . .
+        # # # . .
+        # # # . .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showString("turn left")
+})
